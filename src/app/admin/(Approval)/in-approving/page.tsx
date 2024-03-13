87b8d7bb8d4c1d82cef28eeabc0607157ac96d9b
PhucJoseph <thmphuc0805@gmail.com>
2024-03-13 11:56:40 +0700

    filter done

--- src/app/admin/(Approval)/in-approving/page.tsx
+++ src/app/admin/(Approval)/in-approving/page.tsx
@@ -1,11 +1,49 @@
 import React from 'react'
 import Filter from '../../../ui/admin/filter'
 
+const tempData = [
+  {
+    name: 'Trương Samule',
+    MSSV: '20127610',
+    Year: '2020',
+    Email: 'tsamule@gmail.com',
+    link: 'https://facebook.com/',
+    linkImg: './demo.jpg',
+  },
+  {
+    name: 'Trương Samule',
+    MSSV: '20127610',
+    Year: '2020',
+    Email: 'tsamule@gmail.com',
+    link: 'https://facebook.com/',
+    linkImg: './demo.jpg',
+  },
+  {
+    name: 'Trương Samule',
+    MSSV: '20127610',
+    Year: '2020',
+    Email: 'tsamule@gmail.com',
+    link: 'https://facebook.com/',
+    linkImg: './demo.jpg',
+  },
+  {
+    name: 'Trương Samule',
+    MSSV: '20127610',
+    Year: '2020',
+    Email: 'tsamule@gmail.com',
+    link: 'https://facebook.com/',
+    linkImg: './demo.jpg',
+  },
+]
+
 export default function page() {
   return (
-    <div className="m-auto w-[80vw] flex flex-col bg-[#fafcfe] mt-[3.5vw]">
+    <div className="m-auto w-[78vw] flex flex-col bg-[#fafcfe] mt-[3.5vw] gap-y-3 p-4">
+      <p className="text-gray-900 font-bold text-lg lg:text-[1.5vw]">
+        Yêu cầu xét duyệt cựu sinh viên - #5
+      </p>
       <Filter />
-      <div>h2</div>
+      <div></div>
     </div>
   )
 }
--- src/app/ui/admin/filter.tsx
+++ src/app/ui/admin/filter.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React from 'react'
-import { Input, Button } from '@material-tailwind/react'
+import { Input, Button, Radio } from '@material-tailwind/react'
+import { ChevronDown } from 'react-bootstrap-icons'
 
 const filerBtn = [
   { name: 'Mới nhất', filter: [{ sub: 'Mới nhất' }, { sub: 'Cũ nhất' }] },
@@ -9,16 +10,39 @@ const filerBtn = [
   { name: 'Năm tốt nghiệp', filter: [{ sub: 'A tới Z' }, { sub: 'Z tới A' }] },
 ]
 
-const Icon = () => {
+const IconAtoZ = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="16"
       height="16"
       fill="currentColor"
-      className="bi bi-caret-down-fill"
+      className="bi bi-sort-alpha-down"
       viewBox="0 0 16 16">
-      <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+      <path
+        fill-rule="evenodd"
+        d="M10.082 5.629 9.664 7H8.598l1.789-5.332h1.234L13.402 7h-1.12l-.419-1.371zm1.57-.785L11 2.687h-.047l-.652 2.157z"
+      />
+      <path d="M12.96 14H9.028v-.691l2.579-3.72v-.054H9.098v-.867h3.785v.691l-2.567 3.72v.054h2.645zM4.5 2.5a.5.5 0 0 0-1 0v9.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L4.5 12.293z" />
+    </svg>
+  )
+}
+
+const IconZtoA = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="currentColor"
+      className="bi bi-sort-alpha-up-alt"
+      viewBox="0 0 16 16">
+      <path d="M12.96 7H9.028v-.691l2.579-3.72v-.054H9.098v-.867h3.785v.691l-2.567 3.72v.054h2.645z" />
+      <path
+        fill-rule="evenodd"
+        d="M10.082 12.629 9.664 14H8.598l1.789-5.332h1.234L13.402 14h-1.12l-.419-1.371zm1.57-.785L11 9.688h-.047l-.652 2.156z"
+      />
+      <path d="M4.5 13.5a.5.5 0 0 1-1 0V3.707L2.354 4.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.5.5 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L4.5 3.707z" />
     </svg>
   )
 }
@@ -26,6 +50,15 @@ const Icon = () => {
 export default function Filter() {
   const [message, setMessage] = React.useState('')
 
+  //delete all selected radio options
+  function handleClick() {
+    let allRadioButtons = document.querySelectorAll('#radio')
+    allRadioButtons.forEach((radioButton) => {
+      if (radioButton instanceof HTMLInputElement) {
+        radioButton.checked = false
+      }
+    })
+  }
   return (
     <>
       <Input
@@ -44,20 +77,57 @@ export default function Filter() {
         }}
         type="text"
       />
-      <div className="flex mt-3 gap-2">
-        <Button variant="outlined" placeholder={undefined} size="sm">
+      <div className="flex mt-3 gap-2 ">
+        <Button
+          onClick={handleClick}
+          variant="outlined"
+          placeholder={undefined}
+          size="sm"
+          className="h-fit">
           Xóa bộ lọc
         </Button>
         {filerBtn.map(({ name, filter }) => (
-          <Button
-            variant="outlined"
-            placeholder={undefined}
-            size="sm"
-            className="flex gap-2"
-            key={name}>
-            {name}
-            <Icon />
-          </Button>
+          <div key={name} className="flex-col group ">
+            <Button
+              variant="outlined"
+              placeholder={undefined}
+              size="sm"
+              className="flex gap-2 -mb-1"
+              key={name}>
+              {name}
+              <ChevronDown className="group-hover:rotate-180 " />
+            </Button>
+            <div className="w-[fit] px-3 py-1 ml-3 lg:ml-0 hidden group-hover:flex gap-2 flex-col bg-white rounded-xl font-medium translate-y-1 border-2 border-[var(--secondary)] fixed">
+              {filter.map(({ sub }, idx) => (
+                <div key={idx} className="flex items-center">
+                  <Radio
+                    name={`${name} type`}
+                    color="blue"
+                    crossOrigin={undefined}
+                    key={idx}
+                    label={sub}
+                    id="radio"
+                    className="sticky"
+                  />
+                  {sub === 'Mới nhất' || sub === 'A tới Z' ? (
+                    <IconAtoZ />
+                  ) : (
+                    <IconZtoA />
+                  )}
+                </div>
+              ))}
+              {name === 'Mới nhất' ? (
+                ''
+              ) : (
+                <Input
+                  crossOrigin={undefined}
+                  className=""
+                  placeholder={name}
+                />
+              )}
+              <Button placeholder={undefined}>Lọc</Button>
+            </div>
+          </div>
         ))}
       </div>
     </>
