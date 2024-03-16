4022663230d60a5116eb4c5321c9f731f3a07266
Josehp Tran <tmphuc0805@gmail.com>
2024-03-16 08:55:59 +0700

    clear

--- src/app/admin/(Approval)/approved/page.tsx
+++ src/app/admin/(Approval)/approved/page.tsx
@@ -6,6 +6,7 @@ import { inter, nunito } from '../../../ui/fonts'
 import Link from 'next/link'
 import Image from 'next/image'
 import { Input, Button } from '@material-tailwind/react'
+import CardInformation from '../../../ui/admin/card-information-approved'
 
 const tempData = [
   {
@@ -46,7 +47,7 @@ const tempData = [
   },
 ]
 
-export default function page() {
+export default function Page() {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const [items, setItems] = React.useState(tempData)
   // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -64,81 +65,7 @@ export default function page() {
       </p>
       <Filter />
       <div className="flex flex-wrap gap-5 justify-between mt-5">
-        {items
-          .slice(0, visible)
-          .map(
-            ({ name, MSSV, Year, Email, link, LinkImg, isApproved }, idx) => (
-              <div
-                key={idx}
-                className="w-[100%] lg:w-[49%] py-3 px-2 border-2 border-slate-700 rounded-md">
-                {/* First line include FullName MSSV and Year*/}
-                <div className={`grid grid-cols-4  gap-5 ${inter} mb-2`}>
-                  <div className="flex-col col-span-2">
-                    <p className="font-bold text-[var(--secondary)]   ">
-                      Họ và tên
-                    </p>
-                    <p>{name}</p>
-                  </div>
-                  <div className="flex-col ">
-                    <p className="font-bold text-[var(--secondary)]">MSSV</p>
-                    <p>{MSSV}</p>
-                  </div>
-                  <div className="flex-col ">
-                    <p className="font-bold text-[var(--secondary)]">
-                      Năm nhập học
-                    </p>
-                    <p>{Year}</p>
-                  </div>
-                </div>
-                {/* Second line include Email and link  */}
-                <div className={`grid grid-cols-4  gap-5 ${inter} mb-2`}>
-                  <div className="flex-col col-span-2">
-                    <p className=" font-bold text-[var(--secondary)]">Email</p>
-                    <p>{Email}</p>
-                  </div>
-                  <div className="col-span-1 ">
-                    <p className=" font-bold text-[var(--secondary)]">
-                      Linkedin/Facebook
-                    </p>
-                    <Link href={`${link}`}>{link}</Link>
-                  </div>
-                </div>
-                {/* Final line for the profile image */}
-                <div className="grid grid-cols-3 gap-3">
-                  <Image
-                    src={LinkImg}
-                    alt="profile image"
-                    width={200}
-                    height={200}
-                    className="col-span-1 rounded-full"
-                  />
-                  <form className="col-span-2 py-8 my-auto">
-                    <Input
-                      placeholder="Ghi chú"
-                      crossOrigin={undefined}
-                      className="w-full "
-                      size="lg"
-                    />
-                    <div className="flex justify-center gap-5 mt-3">
-                      {isApproved ? (
-                        <Button
-                          placeholder={undefined}
-                          className={`${nunito} w-52 bg-[var(--blue-02)] font-bold`}>
-                          Phê duyệt
-                        </Button>
-                      ) : (
-                        <Button
-                          placeholder={undefined}
-                          className={`${nunito} w-52 bg-[#ff7373] text-black font-bold`}>
-                          Từ chối
-                        </Button>
-                      )}
-                    </div>
-                  </form>
-                </div>
-              </div>
-            )
-          )}
+        <CardInformation items={items} visible={visible} />
       </div>
       {visible === items.length ? (
         ''
