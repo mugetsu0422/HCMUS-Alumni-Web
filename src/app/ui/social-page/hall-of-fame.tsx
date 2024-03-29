8288a22c9d8564272f33f52f2c2c744e5d633f39
Josehp Tran <tmphuc0805@gmail.com>
2024-03-29 10:12:37 +0700

    clear

--- src/app/ui/social-page/hall-of-fame.tsx
+++ src/app/ui/social-page/hall-of-fame.tsx
@@ -9,9 +9,9 @@ const tempData = [
 
 export default function HallOfFame() {
   return (
-    <div className=" flex flex-col sm:justify-center sm:items-center xl:items-start m-auto sm:w-[500px] xl:w-full max-w-[1110px] h-fit mb-10 gap-y-6">
+    <div className=" flex flex-col sm:justify-center sm:items-center xl:items-start m-auto sm:w-[500px] xl:w-full max-w-[1150px] h-fit mb-10 gap-y-6 p-6">
       <p className="text-3xl font-bold">Gương thành công tiêu biểu</p>
-      <div className="flex sm:flex-col xl:flex-row sm:items-center xl:justify-between w-full">
+      <div className="flex sm:flex-col xl:flex-row sm:items-center xl:items-start xl:justify-evenly w-full">
         {tempData.map(({ name }, idx) => (
           <div
             className="flex flex-col justify-start items-start gap-2"
