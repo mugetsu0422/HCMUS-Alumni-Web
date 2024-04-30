8dfe45fcb30ceca29fcb2c7494b56f8c305842b0
Samuel <20127610@student.hcmus.edu.vn>
2024-04-30 22:37:25 +0700

    integrate comments apis

--- src/app/(social-page)/events/[id]/page.tsx
+++ src/app/(social-page)/events/[id]/page.tsx
@@ -15,6 +15,7 @@ import {
   DialogHeader,
   Avatar,
   Spinner,
+  Textarea,
 } from '@material-tailwind/react'
 import axios from 'axios'
 import { JWT_COOKIE } from '../../../constant'
@@ -25,23 +26,7 @@ import { XLg } from 'react-bootstrap-icons'
 import Link from 'next/link'
 import toast, { Toaster } from 'react-hot-toast'
 import InfiniteScroll from 'react-infinite-scroll-component'
-
-// let listUser = [
-//   {
-//     id: '1',
-//     avatarUrl: '/demo.jpg',
-//     fullName: 'John Smith',
-//   },
-// ]
-
-// // Duplicate elements to n
-// const n = 50
-// listUser = Array(n)
-//   .fill(null)
-//   .flatMap(() => listUser)
-// const fetchMore = () => {
-//   listUser.concat(listUser)
-// }
+import Comments from '../../../ui/social-page/news/comments'
 
 const PARTICIPANT_FETCH_LIMIT = 50
 
@@ -121,7 +106,11 @@ export default function Page({ params }: { params: { id: string } }) {
   const [isDisabled, setIsDisabled] = useState(false)
   const [isLoading, setIsLoading] = useState(true)
   const [participants, setParticipants] = useState([])
+  const [uploadComment, setUploadComment] = useState('')
+  const [comments, setComments] = useState([])
+  const [commentPage, setCommentPage] = useState(0)
 
+  // Event's participants
   const onParticipate = async (eventId) => {
     setIsDisabled(true)
     try {
@@ -175,6 +164,7 @@ export default function Page({ params }: { params: { id: string } }) {
         console.log(e)
       })
   }
+  // Fetch more participants
   const onLoadMoreParticipants = async (page: number) => {
     const res = await axios.get(
       `${process.env.NEXT_PUBLIC_SERVER_HOST}/events/${params.id}/participants?limit=${PARTICIPANT_FETCH_LIMIT}&page=${page}`,
@@ -188,41 +178,122 @@ export default function Page({ params }: { params: { id: string } }) {
     setParticipants(participants.concat(data.participants))
   }
 
+  // Events' comments
+  // Function to handle changes in the textarea
+  const handleUploadCommentChange = (event) => {
+    setUploadComment(event.target.value)
+  }
+  const onUploadComment = (
+    e: React.FormEvent<HTMLFormElement>,
+    parentId: string | null = null,
+    content: string
+  ): void => {
+    e.preventDefault()
+    const comment = {
+      parentId: parentId,
+      content: content,
+    }
+
+    const postCommentToast = toast.loading('Đang đăng')
+    axios
+      .post(
+        `${process.env.NEXT_PUBLIC_SERVER_HOST}/events/${params.id}/comments`,
+        comment,
+        {
+          headers: {
+            Authorization: `Bearer ${Cookies.get(JWT_COOKIE)}`,
+          },
+        }
+      )
+      .then(() => {
+        toast.success('Đăng thành công', { id: postCommentToast })
+      })
+      .catch(() => {
+        toast.error('Đăng thất bại', { id: postCommentToast })
+      })
+  }
+  const onFetchChildrenComments = async (parentId: string) => {
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_SERVER_HOST}/events/comments/${parentId}/children`,
+      {
+        headers: {
+          Authorization: `Bearer ${Cookies.get(JWT_COOKIE)}`,
+        },
+      }
+    )
+    const {
+      data: { comments },
+    } = res
+    return comments
+  }
+  const onFetchComments = () => {
+    setCommentPage((commentPage) => commentPage + 1)
+  }
+
   const handleOpenParticipantsDialog = () => {
     setOpenParticipantsDialog((e) => !e)
   }
 
-  // Fetch event details
+  // Initial fetch
   useEffect(() => {
-    axios
-      .get(`${process.env.NEXT_PUBLIC_SERVER_HOST}/events/${params.id}`, {
+    const detailsPromise = axios.get(
+      `${process.env.NEXT_PUBLIC_SERVER_HOST}/events/${params.id}`,
+      {
         headers: {
           Authorization: `Bearer ${Cookies.get(JWT_COOKIE)}`,
         },
+      }
+    )
+    const isParticipatedPromise = axios.get(
+      `${process.env.NEXT_PUBLIC_SERVER_HOST}/events/is-participated?eventIds=${params.id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${Cookies.get(JWT_COOKIE)}`,
+        },
+      }
+    )
+    const commentsPromise = axios.get(
+      `${process.env.NEXT_PUBLIC_SERVER_HOST}/events/${params.id}/comments`,
+      {
+        headers: {
+          Authorization: `Bearer ${Cookies.get(JWT_COOKIE)}`,
+        },
+      }
+    )
+    Promise.all([detailsPromise, isParticipatedPromise, commentsPromise])
+      .then(([detailsRes, isParticipatedRes, commentsRes]) => {
+        const { data: event } = detailsRes
+        const isParticipated = isParticipatedRes.data[0].isParticipated
+        const { comments } = commentsRes.data
+
+        setIsParticipated(isParticipated)
+        setEvent(event)
+        setComments(comments)
+        setIsLoading(false)
       })
-      .then(({ data: event }) => {
-        // Get list of events that users whether have participated in
-        axios
-          .get(
-            `${process.env.NEXT_PUBLIC_SERVER_HOST}/events/is-participated?eventIds=${params.id}`,
-            {
-              headers: {
-                Authorization: `Bearer ${Cookies.get(JWT_COOKIE)}`,
-              },
-            }
-          )
-          .then(({ data }) => {
-            event.isParticipated = data[0].isParticipated
-            setIsParticipated(data[0].isParticipated)
-            setEvent(event)
-            setIsLoading(false)
-          })
-      })
-      .catch((e) => {
+      .catch((error) => {
         setNoData(true)
       })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  // Fetch more comments
+  useEffect(() => {
+    axios
+      .get(
+        `${process.env.NEXT_PUBLIC_SERVER_HOST}/events/${params.id}/comments?page=${commentPage}`,
+        {
+          headers: {
+            Authorization: `Bearer ${Cookies.get(JWT_COOKIE)}`,
+          },
+        }
+      )
+      .then(({ data: { comments: fetchedComments } }) => {
+        setComments(comments.concat(fetchedComments))
+      })
+      .catch()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [commentPage])
 
   if (noData) {
     return <NoData />
@@ -230,125 +301,168 @@ export default function Page({ params }: { params: { id: string } }) {
 
   if (!isLoading)
     return (
-      <div
-        className={`${nunito.className} flex flex-col gap-6 w-[75%] max-w-[1366px] bg-[--blue-04] rounded-lg m-auto lg:px-10 lg:py-10 mt-16 mb-16`}>
-        <Toaster
-          toastOptions={{
-            success: {
-              style: {
-                background: '#00a700',
-                color: 'white',
+      <>
+        <div
+          className={`${nunito.className} flex flex-col gap-6 w-[75%] max-w-[1366px] bg-[--blue-04] rounded-lg m-auto lg:px-10 lg:py-10 mt-16 mb-16`}>
+          <Toaster
+            toastOptions={{
+              success: {
+                style: {
+                  background: '#00a700',
+                  color: 'white',
+                },
               },
-            },
-            error: {
-              style: {
-                background: '#ea7b7b',
-                color: 'white',
+              error: {
+                style: {
+                  background: '#ea7b7b',
+                  color: 'white',
+                },
               },
-            },
-          }}
-        />
-        <div className="flex flex-col xl:flex-row items-center justify-center m-auto gap-x-10">
-          <img
-            src={event?.thumbnail}
-            alt="image event"
-            className="sm:w-[410px] sm:h-[250px] md:w-[500px] md:h-[350px]  xl:w-[550px] xl:h-[350px]  2xl:w-[40vw] 2xl:h-[400px] max-w-[1000px] object-cover object-center rounded-lg"
+            }}
           />
-          <div className="flex flex-col gap-4 w-full">
-            <div>
-              <p className="text-left sm:text-[1.5rem] 2xl:text-[1.9rem] font-extrabold ">
-                {event?.title}
-              </p>
-              {event?.faculty && (
-                <p className="text-left sm:text-[1.1rem] 2xl:text-[1.5rem] font-semibold">
-                  Khoa {event.faculty.name}
-                </p>
-              )}
-            </div>
-
-            <div className="w-full flex flex-col xl:gap-y-6 2xl:gap-y-10 items-start">
+          <div className="flex flex-col xl:flex-row items-center justify-center m-auto gap-x-10">
+            <img
+              src={event?.thumbnail}
+              alt="image event"
+              className="sm:w-[410px] sm:h-[250px] md:w-[500px] md:h-[350px]  xl:w-[550px] xl:h-[350px]  2xl:w-[40vw] 2xl:h-[400px] max-w-[1000px] object-cover object-center rounded-lg"
+            />
+            <div className="flex flex-col gap-4 w-full">
               <div>
-                <p className="flex text-nowrap items-start gap-2 sm:text-[16px] 2xl:text-[20px]">
-                  <GeoAltFill className="text-[--blue-02]" />{' '}
-                  <span>Địa điểm:</span>
-                  <span className="text-wrap w-[60%]">
-                    {event?.organizationLocation}
-                  </span>
+                <p className="text-left sm:text-[1.5rem] 2xl:text-[1.9rem] font-extrabold ">
+                  {event?.title}
                 </p>
-                <p className="flex items-center gap-2 sm:text-[16px] 2xl:text-[20px]">
-                  <ClockFill className="text-[--blue-02]" />
-                  <span className="w-fit">Thời gian:</span>
-                  <span className="text-wrap w-[60%]">
-                    {event &&
-                      moment(event?.organizationTime).format(
-                        'DD-MM-YYYY HH:mm:ss'
-                      )}
-                  </span>
-                </p>
-
-                {event?.tags && (
-                  <p className="flex items-center gap-2 sm:text-[16px] 2xl:text-[20px]">
-                    <TagFill className="text-[--blue-02]" />
-                    <span className="text-wrap w-[85%]">
-                      {event.tags.map((tag) => tag.name + ' ')}
-                    </span>
+                {event?.faculty && (
+                  <p className="text-left sm:text-[1.1rem] 2xl:text-[1.5rem] font-semibold">
+                    Khoa {event.faculty.name}
                   </p>
                 )}
               </div>
-            </div>
 
-            <div className="w-full flex flex-col justify-center items-center lg:items-start gap-6 ">
-              <div
-                className="flex gap-3 hover:cursor-pointer"
-                onClick={() => {
-                  if (participants.length === 0) {
-                    onInitialLoadParticipants()
-                  }
-                  handleOpenParticipantsDialog()
-                }}>
-                <BarChartFill className="text-[--blue-02] text-[4.1rem]" />
-                <div className="flex flex-col">
-                  <p className="text-[20px] 2xl:text-[30px] font-extrabold">
-                    {event?.participants}
+              <div className="w-full flex flex-col xl:gap-y-6 2xl:gap-y-10 items-start">
+                <div>
+                  <p className="flex text-nowrap items-start gap-2 sm:text-[16px] 2xl:text-[20px]">
+                    <GeoAltFill className="text-[--blue-02]" />{' '}
+                    <span>Địa điểm:</span>
+                    <span className="text-wrap w-[60%]">
+                      {event?.organizationLocation}
+                    </span>
                   </p>
-                  <p className="text-lg">người tham gia</p>
+                  <p className="flex items-center gap-2 sm:text-[16px] 2xl:text-[20px]">
+                    <ClockFill className="text-[--blue-02]" />
+                    <span className="w-fit">Thời gian:</span>
+                    <span className="text-wrap w-[60%]">
+                      {event &&
+                        moment(event?.organizationTime).format(
+                          'DD-MM-YYYY HH:mm:ss'
+                        )}
+                    </span>
+                  </p>
+
+                  {event?.tags && (
+                    <p className="flex items-center gap-2 sm:text-[16px] 2xl:text-[20px]">
+                      <TagFill className="text-[--blue-02]" />
+                      <span className="text-wrap w-[85%]">
+                        {event.tags.map((tag) => tag.name + ' ')}
+                      </span>
+                    </p>
+                  )}
                 </div>
               </div>
-              <ParticipantsDialog
-                openDialog={openParticipantsDialog}
-                handleOpenParticipantsDialog={handleOpenParticipantsDialog}
-                participants={participants}
-                participantCount={event?.participants}
-                onLoadParticipants={onLoadMoreParticipants}
-              />
-              {!isParticipated ? (
-                <Button
-                  onClick={() => onParticipate(params.id)}
-                  disabled={isDisabled}
-                  placeholder={undefined}
-                  size="md"
-                  className="bg-[--blue-02] font-medium w-full text-[16px]">
-                  Tham gia
-                </Button>
-              ) : (
-                <Button
-                  onClick={() => onCancelParticipation(params.id)}
-                  disabled={isDisabled}
-                  placeholder={undefined}
-                  size="md"
-                  className="bg-[--blue-02] font-medium w-full text-[16px]">
-                  Huỷ tham gia
-                </Button>
-              )}
+
+              <div className="w-full flex flex-col justify-center items-center lg:items-start gap-6 ">
+                <div
+                  className="flex gap-3 hover:cursor-pointer"
+                  onClick={() => {
+                    if (participants.length === 0) {
+                      onInitialLoadParticipants()
+                    }
+                    handleOpenParticipantsDialog()
+                  }}>
+                  <BarChartFill className="text-[--blue-02] text-[4.1rem]" />
+                  <div className="flex flex-col">
+                    <p className="text-[20px] 2xl:text-[30px] font-extrabold">
+                      {event?.participants}
+                    </p>
+                    <p className="text-lg">người tham gia</p>
+                  </div>
+                </div>
+                <ParticipantsDialog
+                  openDialog={openParticipantsDialog}
+                  handleOpenParticipantsDialog={handleOpenParticipantsDialog}
+                  participants={participants}
+                  participantCount={event?.participants}
+                  onLoadParticipants={onLoadMoreParticipants}
+                />
+                {!isParticipated ? (
+                  <Button
+                    onClick={() => onParticipate(params.id)}
+                    disabled={isDisabled}
+                    placeholder={undefined}
+                    size="md"
+                    className="bg-[--blue-02] font-medium w-full text-[16px]">
+                    Tham gia
+                  </Button>
+                ) : (
+                  <Button
+                    onClick={() => onCancelParticipation(params.id)}
+                    disabled={isDisabled}
+                    placeholder={undefined}
+                    size="md"
+                    className="bg-[--blue-02] font-medium w-full text-[16px]">
+                    Huỷ tham gia
+                  </Button>
+                )}
+              </div>
             </div>
           </div>
+          <div className="flex flex-col gap-2 w-full">
+            <p className="lg:text-[26px] sm:text-lg font-extrabold">
+              Thông tin chi tiết
+            </p>
+            <p className="text-pretty text-base">{event?.content}</p>
+          </div>
         </div>
-        <div className="flex flex-col gap-2 w-full">
-          <p className="lg:text-[26px] sm:text-lg font-extrabold">
-            Thông tin chi tiết
+
+        <div className="flex flex-col gap-y-2 w-[75%] max-w-[1366px] m-auto mb-10">
+          <form onSubmit={(e) => onUploadComment(e, null, uploadComment)}>
+            <Textarea
+              onChange={handleUploadCommentChange}
+              placeholder={undefined}
+              label="Chia sẻ ý kiến của bạn"
+            />
+            <div className="flex justify-end gap-x-4 pt-2 mr-2">
+              <Button
+                placeholder={undefined}
+                size="md"
+                disabled={!uploadComment.trim()}
+                type="submit"
+                className={`${nunito.className} py-2 px-4 bg-[var(--blue-05)] normal-case text-md`}>
+                Đăng
+              </Button>
+            </div>
+          </form>
+
+          <p className="text-xl font-semibold">
+            Bình luận{' '}
+            <span className="font-normal">
+              ({event?.childrenCommentNumber})
+            </span>
           </p>
-          <p className="text-pretty text-base">{event?.content}</p>
+          <Comments
+            comments={comments}
+            onUploadComment={onUploadComment}
+            onFetchChildrenComments={onFetchChildrenComments}
+          />
+
+          {comments.length != event?.childrenCommentNumber && (
+            <Button
+              onClick={onFetchComments}
+              className="bg-[--blue-02] normal-case text-sm gap-1"
+              placeholder={undefined}>
+              Tải thêm
+            </Button>
+          )}
         </div>
-      </div>
+      </>
     )
 }
