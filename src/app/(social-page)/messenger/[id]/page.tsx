360834a30ec6655f418628403fca2f5f5dd303e1
Josehp Tran <tmphuc0805@gmail.com>
2024-06-25 23:47:48 +0700

    reponsive for mess

--- src/app/(social-page)/messenger/[id]/page.tsx
+++ src/app/(social-page)/messenger/[id]/page.tsx
@@ -26,95 +26,81 @@ const chatDataTemp = [
   { A: "Yeah. It's great", B: '' },
   { A: '', B: 'Really ?' },
   { A: "I've tried the app", B: '' },
-  { A: 'Hi, Mandy', B: '' },
+  { A: '', B: 'Hi, Samule' },
+  { A: 'Hi, Trần Phúc', B: '' },
 ]
-
-function DisplayChat() {
-  return (
-    <div className="fixed w-[81vw] h-[76%] bottom-[54px] px-4 overflow-x-auto scrollbar-webkit flex flex-col-reverse z-0">
-      {chatDataTemp.map(({ A, B }, idx) => (
-        <div
-          key={idx}
-          className={` items-start gap-x-2 my-3 ${
-            B === '' ? 'flex flex-row-reverse' : 'flex'
-          }`}>
-          <Avatar
-            placeholder={undefined}
-            size="md"
-            src={B === '' ? '/demo.jpg' : '/authentication.png'} //Điều kiện này sau này thay đổi + scr ảnh :((
-            alt="avatar"
-          />
-          <div className="py-2 px-3 h-fit max-w-[150px] text-wrap bg-[var(--hcmus-logo)] text-white text-sm font-light rounded-2xl">
-            <p>{A === '' ? B : A}</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  )
-}
 
 export default function Page() {
   return (
-    <div className="fixed w-full h-screen">
-      <div className="fixed ml-[360px] w-[81vw] h-screen bg-white">
-        <header className="fixed top-0 w-[81vw] bg-[--blue-04] px-4 py-2 border-[#eeeeee] border-b-2 mt-20 z-10">
-          <Link
-            href={`/profile/id/about`}
-            className="flex items-center gap-3 hover:bg-[#cbcbcb] w-fit p-2 rounded-lg">
-            <Badge color="green" placement="bottom-end">
-              <Avatar
-                placeholder={undefined}
-                src="/authentication.png"
-                alt="avatar"
-                size="md"
-              />
-            </Badge>
-
-            <div>
-              <p className="text-md font-bold">Nguyễn Duy</p>
-              <p className="text-sm">Đang hoạt động</p>
-            </div>
-          </Link>
-        </header>
-
-        <DisplayChat />
-
-        <footer className="fixed bottom-0 flex flex-row items-center w-[81vw] gap-2  p-2">
-          <div className="flex">
-            <Button
+    <>
+      <header className="fixed flex top-0 w-full bg-[--blue-04] px-4 py-2 border-[#eeeeee] border-b-2 mt-20 z-10">
+        <Link
+          href={`/profile/id/about`}
+          className="flex items-center gap-3 hover:bg-[#cbcbcb] w-fit p-2 rounded-lg">
+          <Badge color="green" placement="bottom-end">
+            <Avatar
               placeholder={undefined}
-              variant="text"
-              className="p-2 w-fit h-fit">
-              <FontAwesomeIcon
-                icon={faImage}
-                className="text-xl text-[#64748B]"
-              />
-            </Button>
-          </div>
-          <Textarea
-            rows={1}
-            placeholder="Nhập tin nhắn"
-            className="min-h-full border-1 focus:border-transparent rounded-full"
-            containerProps={{
-              className: 'grid h-full',
-            }}
-            labelProps={{
-              className: 'before:content-none after:content-none',
-            }}
-          />
+              src="/authentication.png"
+              alt="avatar"
+              size="md"
+            />
+          </Badge>
+
           <div>
-            <Button
-              placeholder={undefined}
-              variant="text"
-              className="p-2 w-fit h-fit">
-              <FontAwesomeIcon
-                icon={faPaperPlane}
-                className="text-xl text-[#64748B]"
-              />
-            </Button>
+            <p className="text-md font-bold">Trần Phúc</p>
+            <p className="text-sm">Đang hoạt động</p>
           </div>
-        </footer>
+        </Link>
+      </header>
+
+      <div className="relative w-full h-full px-4 overflow-x-auto scrollbar-webkit flex flex-col-reverse z-0 mt-20">
+        {chatDataTemp.map(({ A, B }, idx) => (
+          <div
+            key={idx}
+            className={` items-start gap-x-2 my-3 ${
+              B === '' ? 'flex flex-row-reverse' : 'flex'
+            }`}>
+            <Avatar
+              placeholder={undefined}
+              size="md"
+              src={B === '' ? '/demo.jpg' : '/authentication.png'} //Điều kiện này sau này thay đổi + scr ảnh :((
+              alt="avatar"
+            />
+            <div className="py-2 px-3 h-fit w-fit max-w-[200px] lg:max-w-[250px] xl:max-w-[300px]  2xl:max-w-[350px] text-wrap bg-[var(--hcmus-logo)] text-white text-sm font-light rounded-2xl">
+              <p>{A === '' ? B : A}</p>
+            </div>
+          </div>
+        ))}
       </div>
-    </div>
+
+      <div className="relative w-full flex-1 flex items-center gap-2 p-2 border-[#eeeeee] border-t-2">
+        <Button
+          placeholder={undefined}
+          variant="text"
+          className="p-2 w-fit h-fit">
+          <FontAwesomeIcon icon={faImage} className="text-xl text-[#64748B]" />
+        </Button>
+        <Textarea
+          rows={1}
+          placeholder="Nhập tin nhắn"
+          className="min-h-full border-1 focus:border-transparent rounded-full bg-[#f5f5f5] grow-0"
+          containerProps={{
+            className: 'grid h-full',
+          }}
+          labelProps={{
+            className: 'before:content-none after:content-none',
+          }}
+        />
+        <Button
+          placeholder={undefined}
+          variant="text"
+          className="p-2 w-fit h-fit">
+          <FontAwesomeIcon
+            icon={faPaperPlane}
+            className="text-xl text-[#64748B]"
+          />
+        </Button>
+      </div>
+    </>
   )
 }
--- src/app/(social-page)/messenger/layout.tsx
+++ src/app/(social-page)/messenger/layout.tsx
@@ -1,6 +1,5 @@
 'use client'
-import React from 'react'
-import Sidebar from '@/app/ui/social-page/messenger/sidebar'
+import React, { useEffect, useState } from 'react'
 import { nunito, plusJakartaSans } from '@/app/ui/fonts'
 import { Avatar } from '@material-tailwind/react'
 import Link from 'next/link'
@@ -10,14 +9,48 @@ const dataTemp = [
   { name: 'QQQQQ', time: '11:00', latestMes: '122212213', isRead: true },
   { name: 'QQQQQ', time: '11:00', latestMes: '122212213', isRead: false },
   { name: 'QQQQQ', time: '11:00', latestMes: '122212213', isRead: false },
-  { name: 'QQQQQ', time: '11:00', latestMes: '122212213', isRead: true },
+  {
+    name: 'QQQQQ',
+    time: '11:00',
+    latestMes: '122212213',
+    isOline: true,
+    isRead: true,
+  },
   {
     name: 'QQQQQ',
     time: '11:00',
     latestMes: '121111111111111111',
+    isOline: true,
     isRead: false,
   },
-  { name: 'QQQQQ', time: '11:00', latestMes: '122212213', isRead: true },
+  {
+    name: 'QQQQQ',
+    time: '11:00',
+    latestMes: '122212213',
+    isOline: true,
+    isRead: true,
+  },
+  {
+    name: 'QQQQQ',
+    time: '11:00',
+    latestMes: '111111111111121555555555555555555555555555111111',
+    isOline: true,
+    isRead: true,
+  },
+  {
+    name: 'QQQQQ',
+    time: '11:00',
+    latestMes: '111111111111121555555555555555555555555555111111',
+    isOline: true,
+    isRead: true,
+  },
+  {
+    name: 'QQQQQ',
+    time: '11:00',
+    latestMes: '111111111111121555555555555555555555555555111111',
+    isOline: true,
+    isRead: true,
+  },
   {
     name: 'QQQQQ',
     time: '11:00',
@@ -31,57 +64,74 @@ export default function GroupLayout({
 }: {
   children: React.ReactNode
 }) {
+  const [isSmallerThan2XL, setIsSmallerThan2XL] = useState(true)
+
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      window.innerWidth <= 1024
+        ? setIsSmallerThan2XL(false)
+        : setIsSmallerThan2XL(true)
+    })
+  }, [])
+
   return (
-    <div className="flex relative">
-      <Sidebar>
-        <div className="p-4 gap-y-6 w-full bg-white">
+    <div className="flex flex-1 h-full">
+      <div className="w-1/4 max-w-[340px] h-[90vh] overflow-hidden">
+        <div className="flex flex-col p-4 gap-y-6 w-full overflow-hidden">
           <div className="flex justify-between">
             <p
-              className={`${nunito.className} text-xl xl:text-2xl font-bold text-black pl-4 pr-4 pb-4 pt-2`}>
+              className={`${nunito.className} text-xl xl:text-2xl font-bold text-black pt-2`}>
               Nhắn tin
             </p>
           </div>
           {/* #f7fafd */}
-
           <div className="overflow-y-auto scrollbar-webkit">
-            <div className="flex flex-col gap-3 h-screen w-[17.5rem]">
-              {dataTemp.map(({ name, time, latestMes, isRead }, idx) => (
-                <Link
-                  href={`/messenger/id`}
-                  key={idx}
-                  className={`${plusJakartaSans.className} flex justify-between rounded-lg hover:bg-blue-gray-50 hover:cursor-pointer p-2 w-full`}>
-                  <Avatar
-                    placeholder={undefined}
-                    src="/demo.jpg"
-                    width={50}
-                    height={50}
-                    alt="user avatar"
-                  />
-                  <div className="ml-2 w-[100%] flex justify-around">
-                    <div className="w-[170px]">
-                      <p className="font-bold text-black text-base">{name}</p>
-                      <p
-                        className={`truncate w-[90%] ${
-                          !isRead && 'font-semibold text-[--secondary]'
-                        } text-[14px]`}>
-                        {latestMes}
-                      </p>
-                    </div>
+            <div className="flex flex-col gap-3 h-[91vh] w-fit max-w-[18rem] pr-2">
+              {dataTemp.map(
+                ({ name, time, latestMes, isRead, isOline }, idx) => (
+                  <Link
+                    href={`/messenger/id`}
+                    key={idx}
+                    className={`${plusJakartaSans.className} flex justify-around rounded-lg hover:bg-blue-gray-50 hover:cursor-pointer pt-2 pb-2 pl-2 pr-4 w-full`}>
+                    <Avatar
+                      placeholder={undefined}
+                      src="/demo.jpg"
+                      size="md"
+                      alt="user avatar"
+                    />
+                    {isSmallerThan2XL && (
+                      <div className="ml-2 w-[100%] flex justify-around">
+                        <div className="w-[90vw] max-w-[170px]">
+                          <p className="font-bold text-black text-base">
+                            {name}
+                          </p>
+                          <p
+                            className={`truncate max-w-[90%] ${
+                              !isRead && 'font-semibold text-[--secondary]'
+                            } text-[14px]`}>
+                            {latestMes}
+                          </p>
+                        </div>
 
-                    <div>
-                      <p>{time}</p>
-                      {!isRead && (
-                        <span className="mx-auto mt-1 block h-2 w-2 rounded-full bg-[var(--warning)] content-['']" />
-                      )}
-                    </div>
-                  </div>
-                </Link>
-              ))}
+                        <div>
+                          <p>{time}</p>
+                          {!isRead && (
+                            <span className="mx-auto mt-1 block h-2 w-2 rounded-full bg-[var(--warning)] content-['']" />
+                          )}
+                        </div>
+                      </div>
+                    )}
+                  </Link>
+                )
+              )}
             </div>
           </div>
         </div>
-      </Sidebar>
-      {children}
+      </div>
+
+      <main className=" flex-1 w-3/4 h-[91vh] flex flex-col items-between border-[#eeeeee] border-l-2 overflow-hidden">
+        {children}
+      </main>
     </div>
   )
 }
