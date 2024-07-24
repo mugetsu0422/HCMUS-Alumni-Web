ff3a9127ec95119ad828709f6bb33de3824fe8b0
Samuel <20127610@student.hcmus.edu.vn>
2024-07-24 18:38:15 +0700

    Update page.tsx

--- src/app/(social-page)/notifications/page.tsx
+++ src/app/(social-page)/notifications/page.tsx
@@ -78,6 +78,7 @@ export default function Page() {
           {notifications.map((notification) => (
             <NotificationItem
               notification={notification}
+              onHandleOpenNotification={() => {}}
               key={notification.id}
             />
           ))}
